2473e78f546ef55541636b0defece98b5e4b9a0a
Tai Ming <taimingsama@gmail.com>
2025-06-14 14:29:12 +0800

    Add additional color outputs to Program.java

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,5 +3,7 @@ public class Program
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println("black");
+        System.out.println("white");
     }
 }
