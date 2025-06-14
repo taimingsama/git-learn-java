a8aa698ec644102b400c5d6e5d216deb54828fd2
Tai Ming <taimingsama@gmail.com>
2025-06-14 15:25:33 +0800

    Revert "Add output for "one" in Program.java"
    This reverts commit c629c56ee5711e6c73c3c2ad78b65dc40ea510dc.

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,6 +5,5 @@ public class Program
         System.out.println("Hello World!");
         System.out.println("red");
         System.out.println("blue");
-        System.out.println("one");
     }
 }
